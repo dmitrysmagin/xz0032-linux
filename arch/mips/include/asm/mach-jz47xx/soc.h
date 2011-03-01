bcf7a7eeb3ac42509c97de9e2f01676e9eb4029a
Lars-Peter Clausen <lars@metafoo.de>
2011-03-01 13:03:16 +0100

    MIPS: JZ4740: Refactor JZ4740 platform to support otherJZ47xx SoCs

---
+++ arch/mips/include/asm/mach-jz47xx/soc.h
@@ -0,0 +1,13 @@
+#ifndef __ASM_MACH_JZ4740_SOC_H__
+#define __ASM_MACH_JZ4740_SOC_H__
+
+static inline bool soc_is_jz4740(void)
+{
+#ifdef MACH_JZ4740
+	return true;
+#else
+	return false;
+#endif
+}
+
+#endif
