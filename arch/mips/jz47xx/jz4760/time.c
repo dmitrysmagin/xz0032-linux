184d4914225fec620e05763703c0073aa0d2cfd5
Lars-Peter Clausen <lars@metafoo.de>
2011-03-05 00:18:59 +0100

    Add OST support
    Add support for the OST found on the JZ4750 and later.
    Use it instead of the timer clocksource and clockevent for JZ4750 and JZ4760.

--- arch/mips/jz47xx/jz4760/time.c
+++ arch/mips/jz47xx/jz4760/time.c
@@ -3,12 +3,10 @@
 #include <asm/mach-jz47xx/jz4760/irq.h>
 
 #include "../timer.h"
-#include "../timer-csrc.h"
-#include "../timer-cevt.h"
+#include "../ost.h"
 
 void __init plat_time_init(void)
 {
 	jz4740_timer_init();
-	jz47xx_timer_csrc_init(0);
-	jz47xx_timer_cevt_init(JZ4760_IRQ_TCU1, 5);
+	jz47xx_ost_init(JZ4760_IRQ_TCU0);
 }
