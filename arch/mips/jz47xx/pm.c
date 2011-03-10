3a077bbc3168e91639e65a3f93998b5508065b3e
Peter Zotov <whitequark@whitequark.org>
2011-03-10 18:50:33 +0300

    Jz47xx: fix PM driver.

--- arch/mips/jz47xx/pm.c
+++ arch/mips/jz47xx/pm.c
@@ -18,39 +18,39 @@
 #include <linux/delay.h>
 #include <linux/suspend.h>
 
-#include <asm/mach-jz47xx/jz4740/clock.h>
+#include <clock.h>
 
 #include "clock.h"
 #include "irq.h"
 
-static int jz4740_pm_enter(suspend_state_t state)
+static int jz47xx_pm_enter(suspend_state_t state)
 {
 	jz4740_intc_suspend();
-	jz4740_clock_suspend();
+	jz47xx_clock_suspend();
 
-	jz4740_clock_set_wait_mode(JZ4740_WAIT_MODE_SLEEP);
+	jz47xx_clock_set_wait_mode(JZ47XX_WAIT_MODE_SLEEP);
 
 	__asm__(".set\tmips3\n\t"
 		"wait\n\t"
 		".set\tmips0");
 
-	jz4740_clock_set_wait_mode(JZ4740_WAIT_MODE_IDLE);
+	jz47xx_clock_set_wait_mode(JZ47XX_WAIT_MODE_IDLE);
 
-	jz4740_clock_resume();
+	jz47xx_clock_resume();
 	jz4740_intc_resume();
 
 	return 0;
 }
 
-static const struct platform_suspend_ops jz4740_pm_ops = {
+static const struct platform_suspend_ops jz47xx_pm_ops = {
 	.valid		= suspend_valid_only_mem,
-	.enter		= jz4740_pm_enter,
+	.enter		= jz47xx_pm_enter,
 };
 
-static int __init jz4740_pm_init(void)
+static int __init jz47xx_pm_init(void)
 {
-	suspend_set_ops(&jz4740_pm_ops);
+	suspend_set_ops(&jz47xx_pm_ops);
 	return 0;
 
 }
-late_initcall(jz4740_pm_init);
+late_initcall(jz47xx_pm_init);
